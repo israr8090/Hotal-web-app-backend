0bb596e8b69df85b16bc8ec1c3f51db664aedcc5
israr8090 <mdistata772@gmail.com>
2024-05-06 22:29:23 +0530

    added .env file

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-var mongoURL = 'mongodb+srv://israr8090:n2AfpEj2o3T89ULx@israrcluster.xqlk17h.mongodb.net/baba-room';
+// var mongoURL = 'mongodb+srv://israr8090:n2AfpEj2o3T89ULx@israrcluster.xqlk17h.mongodb.net/baba-room';
 
 const connectAsyncAwait = async () => {
     try {
-        const resp = await mongoose.connect(mongoURL);
+        const resp = await mongoose.connect(process.env.MONGO_URL);
+        // const resp = await mongoose.connect(mongoURL);
         console.log('connected with DB Successfully');
     } catch (error) {
         console.log(`Error Occored ${error}`)
